2882dc2a04eb9d7f722867e0b6f4a698dbf95b98
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-03-05 14:25:46 -0300

    criando metodo map

--- app/main.js
+++ app/main.js
@@ -1,34 +1,10 @@
-let livros = [];
-const endpontDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
+let livros = []
+const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
 getBuscarLivrosDaAPI()
-const elementoParaInserirLivros = document.getElementById('livros')
 
-//trazer as informações do endpoint para a aplicação
 async function getBuscarLivrosDaAPI() {
-
-    const res = await fetch(endpontDaAPI)
+    const res = await fetch(endpointDaAPI)
     livros = await res.json()
-    console.table(livros)
-
-    exibirOsLivrosNaTela(livros)
+    livros = aplicarDesconto(livros)
+    exibirOsLivrosNaTela(livrosComDesconto)
 }
-
-function exibirOsLivrosNaTela(listaDeLivros) {
-    listaDeLivros.forEach(livro => {
-        elementoParaInserirLivros.innerHTML += `
-        <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-        <h2 class="livro__titulo">
-          ${livro.titulo}
-        </h2>
-        <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">${livro.preco}</p>
-        <div class="tags">
-          <span class="tag">${livro.categoria}</span>
-        </div>
-      </div>`
-    });
-}
-
-
-
